5a28a7986233e647b20ce2a61da5d6c9be773fc0
smun <smun@student.42seoul.kr>
2020-07-08 14:11:50 +0900

    결과 출력 여부를 선택할 수 있도록 수정.

--- test.sh
+++ test.sh
@@ -7,82 +7,73 @@ echo
 #   ver 1.  first                                    2020-07-08 12:59
 
 export PROJECT=$1
+export PRINT_RESULT=$2
 
 find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
 
-prepare_c_exercise() {
-    echo
-    echo " === Test Exercise $1 === "
-    cd ex$1
-    echo " >> Norminette Result <<"
-    norminette -R CheckForbiddenSourceHeader
-}
 test_c_exercise() {
-    gcc -Wall -Wextra -Werror -o test_ex$1 *.c ../$PROJECT/ex$1.c
+    echo " === Test Exercise $1 === "
+    DIR=$(dirname "$BASH_SOURCE")
+    gcc -Wall -Wextra -Werror -o test_ex$1 ex$1/*.c $DIR/$PROJECT/ex$1.c
     ./test_ex$1 >> utest
-
-    if [[ "$2" != "NotPrint" ]]
+    if [[ $PRINT_RESULT == "p" ]]
     then
         echo " >> Your result <<"
         ./test_ex$1
         echo
         echo " >> Expected result <<"
-        cat ../$PROJECT/ex$1.result
+        cat $DIR/$PROJECT/ex$1.result
         echo
-    else
-        echo " >>!! Hidden result print !!<<"
     fi
 
     echo " >> diff result <<"
-    diff -U 3 ../$PROJECT/ex$1.result utest | cat -e
+    diff -U 3 $DIR/$PROJECT/ex$1.result utest | cat -e
     echo
-    echo "  *** Your Exercise $1 result is... :/ ***"
-    DIFF_RESULT=$(diff -U 3 ../$PROJECT/ex$1.result utest | cat -e)
+    DIFF_RESULT=$(diff -U 3 $DIR/$PROJECT/ex$1.result utest | cat -e)
     if [[ $DIFF_RESULT == "" ]]
     then
-        echo "       Diff OK :D"
+        echo " Diff OK :D"
     else
-        echo "       Diff KO :("
+        echo " Diff KO :("
     fi
 
     echo
     rm -rf test_ex$1 utest
-    cd ..
 }
 
 if [[ $PROJECT == "C00" ]]
 then
+    norminette -R CheckForbiddenSourceHeader
     for I in {0..8}
     do
-        prepare_c_exercise $(printf "%02d" "$I")
         test_c_exercise $(printf "%02d" "$I")
     done
 elif [[ $PROJECT == "C01" ]]
 then
+    norminette -R CheckForbiddenSourceHeader
     for I in {0..8}
     do
-        prepare_c_exercise $(printf "%02d" "$I")
         test_c_exercise $(printf "%02d" "$I")
     done
 elif [[ $PROJECT == "C02" ]]
 then
+    norminette -R CheckForbiddenSourceHeader
     for I in {0..12}
     do
-        prepare_c_exercise $(printf "%02d" "$I")
         test_c_exercise $(printf "%02d" "$I")
     done
 elif [[ $PROJECT == "C03" ]]
 then
+    norminette -R CheckForbiddenSourceHeader
     for I in {0..5}
     do
-        prepare_c_exercise $(printf "%02d" "$I")
         test_c_exercise $(printf "%02d" "$I")
     done
 elif [[ $PROJECT == "C04" ]]
 then
+    norminette -R CheckForbiddenSourceHeader
     for I in {0..2}
     do
-        prepare_c_exercise $(printf "%02d" "$I")
         test_c_exercise $(printf "%02d" "$I")
     done
 fi
