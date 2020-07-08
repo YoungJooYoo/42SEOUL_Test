98c4109c83296f676c3a32c55304f9c88d212013
smun <smun@student.42seoul.kr>
2020-07-08 19:39:57 +0900

    Fix for ex03

--- C04/ex03.c
+++ C04/ex03.c
@@ -17,5 +17,12 @@ int main(void)
 	printf("%d\n", ft_atoi("\t\v----------++5123145"));
 	printf("%d\n", ft_atoi("\n\f----------+-+5123145"));
 	printf("%d\n", ft_atoi("\r     \n\t\v\n\f---++-----++2123133345"));
+
+	printf("%d\n", ft_atoi("    +----++-2147483asdas648"));
+	printf("%d\n", ft_atoi("+------+10000001"));
+	printf("%d\n", ft_atoi(" +-----------+00000000000000000000000\t\v\f\n 000000000000000000"));
+	printf("%d\n", ft_atoi("\t\v----------++5123--145"));
+	printf("%d\n", ft_atoi("\n\f----------+-+5123+145"));
+	printf("%d\n", ft_atoi("\r     \n\t\v\n\f---++-----++212  3133345"));
 	return 0;
 }
