04a66e475b30a2255e57a661266852a614801884
smun <smun@student.42seoul.kr>
2020-07-22 21:48:08 +0900

    fix test case

--- C11/ex04.c
+++ C11/ex04.c
@@ -10,12 +10,12 @@ int		ft_compare(int n1, int n2)
 int main(void)
 {
 	int sorted1[] = {10, 30, 56, 1235, 6546, 10345};
-	int sorted2[] = {10345, 6546, 1235, 56, 30, 10};
+	int sorted2[] = {7, 7, 7, 7, 6, 6, 6, 6, 5, 5, 4, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 1, 1, 1};
 	int not_sorted1[] = {10, 30, 56, 3, 10345, 1235, 6546};
 	int not_sorted2[] = {6546, 1235, 10345, 3, 56, 30, 10};
 	printf("%d\n", ft_is_sort(sorted1, 6, &ft_compare));
-	printf("%d\n", ft_is_sort(sorted2, 6, &ft_compare));
-	printf("%d\n", ft_is_sort(not_sorted1, 6, &ft_compare));
-	printf("%d\n", ft_is_sort(not_sorted2, 6, &ft_compare));
+	printf("%d\n", ft_is_sort(sorted2, 26, &ft_compare));
+	printf("%d\n", ft_is_sort(not_sorted1, 7, &ft_compare));
+	printf("%d\n", ft_is_sort(not_sorted2, 7, &ft_compare));
 	return (0);
 }
--- C11/ex07.c
+++ C11/ex07.c
@@ -1,42 +1,55 @@
 #include <stdio.h>
+#include <string.h>
 
 void	ft_advanced_sort_string_tab(char **tab, int (*cmp)(char *, char *));
 
-int		ft_strcmp(char *s1, char *s2)
+int		r_strcmp(char *s1, char *s2)
 {
-	while (*s1 || *s2)
-	{
-		if (*s1 != *s2)
-			return (*(unsigned char*)s1 - *(unsigned char*)s2);
-		s1++;
-		s2++;
-	}
-	return (0);
+	return strcmp(s1, s2);
 }
 
-int main(void)
+int		compare_len(char *s1, char *s2)
+{
+	return strlen(s1) - strlen(s2);
+}
+
+void test1(int (*f)(char*,char*))
 {
 	char *tab[] = {
-		"Hello hello hello",
-		"Hello 11234",
-		"",
-		"Go Thourh",
-		"134",
-		"!!!!!",
-		"\?!\?!\?!\?\?!\?!\?!",
-		"::*#$@Hell",
-		"42 Seoul La Piscine",
-		"dk whfflek",
-		"anjsrk tmftmf whfflsmsrj rkxdmsep zz",
-		"gma ;;",
+		"rXGUhQt",
+		"VihSZbMNyY",
+		"aMoCh29W8B",
+		"h6kV4",
+		"R",
+		"GiJl9",
+		"sMCryRJPSOu6",
+		"FNb1",
+		"oU",
+		"oE",
+		"d",
+		"A",
+		"PM",
+		"NDmFi",
+		"JtDHW",
+		"B1JKR",
+		"GTIurC0teHoJ",
+		"VE",
+		"N4ZS",
 		0
 	};
-	ft_advanced_sort_string_tab(tab, &ft_strcmp);
+	ft_advanced_sort_string_tab(tab, f);
 	int i = 0;
 	while (tab[i])
 	{
 		printf("%s\n", tab[i]);
 		i++;
 	}
+	printf("\n");
+}
+
+int main(void)
+{
+	test1(&compare_len);
+	test1(&r_strcmp);
 	return (0);
 }
